92d106286f5382926a773eed94c03893aa6a93e8
Zhou Li <zhoulijoe@gmail.com>
2016-01-26 16:25:56 +0800

    Add more field to model serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -5,5 +5,9 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(source='get_full_name', read_only=True)
+
     class Meta:
         model = get_user_model()
+        fields = ('id', 'full_name', 'groups')
+        depth = 1
