f3e3ae1f93f66d601c5eb7d32f213a9e9e500ccd
Zhou Li <zhoulijoe@gmail.com>
2016-01-26 12:46:33 +0800

    Create rest view to get users

---
+++ accounts/serializers.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
